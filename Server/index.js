96687ed26db83cda63f39399ca35a4c04a90bd28
eugene <eugene.s@nexttehnika.ru>
2021-03-09 11:58:36 +1000

    add echo to server

--- Server/index.js
+++ Server/index.js
@@ -3,14 +3,26 @@ const app = express()
 const port = 8080
 const host = '127.0.0.1'
 const pathSt = '/static'
+const echoPath = '/echo'
 
-app.use(pathSt, function(req, res, next){
-    if ('POST' != req.method){
+app.use(pathSt, function (req, res, next) {
+    if ('POST' != req.method) {
         next()
-    }else{
+    } else {
         req.method = 'GET'
         next()
     }
 })
 app.use(pathSt, express.static('static'));
+app.all(echoPath, (req, res) => {
+    res.contentType(req.header("content-type"));
+    res.flushHeaders();
+    req.on('data', chunk => {
+        console.log('' + chunk);
+        res.write(chunk);
+    })
+    req.on('end', () => {
+        res.end();
+    })
+})
 app.listen(port, host)
