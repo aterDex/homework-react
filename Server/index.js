b8781f1e45029eff74c3532bb504d57fef09e28e
eugene <eugene.s@nexttehnika.ru>
2021-03-09 11:33:32 +1000

    setting server express

--- Server/index.js
+++ Server/index.js
@@ -1,27 +1,16 @@
-const http = require('http')
-
-const server = http.createServer(function (request, response) {
-    console.dir(request.param)
-
-    if (request.method == 'POST') {
-        console.log('POST')
-
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.end('{"a": 100}');
-    } else {
-        console.log('GET')
-        var html = `
-            <html>
-                <body>
-                   <h1>Empty</h1>
-                </body>
-            </html>`
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.end(html)
-    }
-})
-
+const express = require('express')
+const app = express()
 const port = 8080
 const host = '127.0.0.1'
-server.listen(port, host)
-console.log(`Listening at http://${host}:${port}`)
+const pathSt = '/static'
+
+app.use(pathSt, function(req, res, next){
+    if ('POST' != req.method){
+        next()
+    }else{
+        req.method = 'GET'
+        next()
+    }
+})
+app.use(pathSt, express.static('static'));
+app.listen(port, host)
