c6b812bb6dbf7188f7742eec46c8f6e9307dc901
eugene <eugene.s@nexttehnika.ru>
2021-03-12 15:59:09 +1000

    create base react + bootstrap + bootstrap-icon

---
+++ hw89-jsx/src/components/app/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import PostStatusFilter from "../post-status-filter";
+import PostList from "../post-list";
+import PostAddForm from "../post-add-form";
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import './app.css';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList/>
+            <PostAddForm/>
+        </div>
+    );
+}
+
+export default App;
