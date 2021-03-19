6b951f893c8f3c4fa3001ab8523ab24bbc671312
eugene <eugene.s@nexttehnika.ru>
2021-03-19 16:41:01 +1000

    add hoc

--- hw96-middle/src/components/itemList/itemList.js
+++ hw96-middle/src/components/itemList/itemList.js
@@ -3,48 +3,13 @@ import './itemList.css';
 import Spinner from "../spinner";
 import PropTypes from "prop-types";
 
-export default class ItemList extends React.Component {
-
-    static props = {
-        onItemSelected: () => {
-        },
-        renderItem: () => {
-        }
-    }
-
-    static propTypes = {
-        onItemSelected: PropTypes.func,
-        renderItem: PropTypes.func,
-        onData: PropTypes.func.isRequired
-    }
-
-    state = {
-        itemList: null
-    }
-
-    componentDidMount() {
-        const {onData} = this.props;
-        onData()
-            .then(itemList => {
-                this.setState({itemList});
-            }).catch(e => this.onError(e));
-    }
-
-    onError(e) {
-        this.props.onError(e);
-    }
-
+class ItemList extends React.Component {
     render() {
-        const {onItemSelected} = this.props;
-        const {renderItem} = this.props;
-        const {itemList} = this.state;
-
-        const wait = itemList ? null : (<li key="spinner_li_qcdkpwe21" className="list-group-item"><Spinner/></li>)
-        const body = itemList ? itemList.map(item => <ItemView key={item.id} item={item} renderItem={renderItem}
+        const {onItemSelected, renderItem, data} = this.props;
+        const body = data ? data.map(item => <ItemView key={item.id} item={item} renderItem={renderItem}
                                                                onClick={x => onItemSelected(x)}/>) : null;
         return (
             <ul className="item-list list-group">
-                {wait}
                 {body}
             </ul>
         );
@@ -54,3 +19,48 @@ export default class ItemList extends React.Component {
 const ItemView = ({item, onClick, renderItem}) => {
     return (<li className="list-group-item" onClick={x => onClick(item)}>{renderItem(item)}</li>)
 }
+
+const withData = (View) => {
+    return class extends React.Component {
+
+        static props = {
+            onItemSelected: () => {
+            },
+            renderItem: () => {
+            }
+        }
+
+        static propTypes = {
+            onItemSelected: PropTypes.func,
+            renderItem: PropTypes.func,
+            onData: PropTypes.func.isRequired
+        }
+
+        state = {
+            data: null
+        }
+
+        componentDidMount() {
+            const {onData} = this.props;
+            onData()
+                .then(data => {
+                    this.setState({data});
+                }).catch(e => this.onError(e));
+        }
+
+        onError(e) {
+            this.props.onError(e);
+        }
+
+        render() {
+            const {data} = this.state;
+            const wait = data ? null : (<li key="spinner_li_qcdkpwe21" className="list-group-item"><Spinner/></li>)
+            if (wait) {
+                return wait;
+            }
+            return <View {...this.props} data={data}/>;
+        }
+    };
+}
+
+export default withData(ItemList);
