2f3679bacd3a184800134f84275edc482f8aaf6b
eugene <eugene.s@nexttehnika.ru>
2021-03-19 17:51:21 +1000

    redux base

--- hw105-redux/src/index.js
+++ hw105-redux/src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello redux')
+import {createStore} from 'redux';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return ++state;
+        case 'DEC':
+            return --state;
+        case 'RND':
+            return state + action.rnd;
+        default:
+            return state;
+    }
+}
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = () => ({type: 'RND', rnd: Math.floor(Math.random() * 10)});
+
+
+const store = createStore(reducer);
+const counter = document.querySelector('#counter');
+
+store.subscribe(() => {
+    counter.textContent = store.getState();
+})
+
+document.querySelector('#dec').addEventListener('click', () => store.dispatch(dec()));
+document.querySelector('#inc').addEventListener('click', () => store.dispatch(inc()));
+document.querySelector('#rnd').addEventListener('click', () => store.dispatch(rnd()));
