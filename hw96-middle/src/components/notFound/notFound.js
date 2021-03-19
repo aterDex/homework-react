e7d52fc3a8197ddbbbdf2d084380cbd6a9743337
eugene <eugene.s@nexttehnika.ru>
2021-03-19 14:45:38 +1000

    add not found page

--- hw96-middle/src/components/notFound/notFound.js
+++ hw96-middle/src/components/notFound/notFound.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 export default class NotFound extends React.Component {
     render() {
         return (<BasePane>
-            <Button><Link to='/'>Click for return</Link></Button>
+            <div className="d-flex justify-content-center"><Button><Link to='/'>Not found, click for return</Link></Button></div>
         </BasePane>)
     }
 }
